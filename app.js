e9f55954d0f066b9755626842bd98006caf8d235
PEdbot <vinicius.tamae@pedbot.com.br>
2023-12-27 17:56:56 -0300

    Arrumando a Logica

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 const shiny = document.querySelector('#shiny');
 const imgPokemon = document.querySelector('#imgPokemon');
+const pokmeon = document.querySelector('#selectPokemon')
+const namePokemon = document.querySelector('#namePokemon')
+const IdPokemon = document.querySelector('#IdPokemon')
 let countShiny = true;
+
+
+const searchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+};
+
+const infoPokmeon = async (pokemon) => {
+    const data = await searchPokemon(pokemon)
+    namePokemon.innerHTML = data.name
+    IdPokemon.innerHTML = data.id
+
+}
+
+
 
 shiny.addEventListener('click', () => {
     if (countShiny) {
-        imgPokemon.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png';
+        imgPokemon.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/1.gif';
     } else {
-        imgPokemon.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+        imgPokemon.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif';
     }
-
     countShiny = !countShiny;
 });
+
+
+// infoPokmeon('1')
+
+
+
+
+
+
+
+
 
 
 
